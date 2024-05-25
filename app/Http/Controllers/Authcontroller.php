6e43b16069423a7a95cc62c46ab4607054251624
arfnc <150315240+arfnc@users.noreply.github.com>
2024-05-25 22:04:36 +0700

    fina3

--- app/Http/Controllers/Authcontroller.php
+++ app/Http/Controllers/Authcontroller.php
@@ -9,15 +9,69 @@ use Illuminate\Http\RedirectResponse;
 
 class AuthController extends Controller
 {
-    public function postLogin(Request $request)
+    // masih coba dan fix bisa
+    public function postLogin(Request $request): RedirectResponse
     {
-        $credentials = $request->only('email', 'password');
+        $credentials = $request->validate([
+            'email' => ['required'],
+            'password' => ['required'],
+        ]);
 
-        if (Auth::attempt($credentials, $request->filled('remember'))) {
-            // Authentication passed...
-            return redirect()->intended('dashboard'); // Change 'dashboard' to your desired redirect route
+        if (Auth::attempt($credentials)) {
+            $request->session()->regenerate();
+
+            if (Auth::user()->roles === 'administrator' || Auth::user()->roles === 'kaprodi') {
+                return redirect()->intended('admin');
+            } elseif (Auth::user()->roles === 'dosen' || Auth::user()->roles === 'mahasiswa') {
+                return back()->withErrors([
+                    'login_error' => 'Dosen dan Mahasiswa tidak dapat login di halaman login khusus Administrator dan Ketua Program Studi.',
+                ]);
+            }
         } else {
-            return redirect()->back()->withErrors(['Invalid credentials']);
+            return back()->withErrors([
+                'email' => 'Email atau password anda salah.',
+            ])->onlyInput('email');
         }
+    }
+
+    // yang lama dan bisa dipakai, barangkali yang baru ada error bisa pakai ini untuk jaga2!
+    // public function postLogin(Request $request): RedirectResponse
+    // {
+    //     $credentials = $request->validate([
+    //         'email' => ['required'],
+    //         'password' => ['required'],
+    //     ]);
+
+    //     if (Auth::attempt($credentials)) {
+    //         $request->session('')->regenerate();
+    //         // return redirect()->intended('admin/login');
+    //         if (Auth::user()->roles == 'administrator' && 'kaprodi') {
+    //             return redirect()->intended('admin');
+    //         }
+    //         // masih testing, nanti sesuaikan intended ke page login khusus dosen & mhs
+    //         elseif (Auth::user()->level == 'dosen' && 'mahasiswa') {
+    //             return redirect()->intended('admin/login');
+    //         }
+    //     }
+
+    //     return back()->withErrors([
+    //         'email' => 'Email atau password anda salah.',
+    //     ])->onlyInput('email');
+    // }
+
+    public function logout()
+    {
+        Auth::logout();
+        return redirect('admin/login');
+    }
+
+    public function showusername()
+    {
+        if (Auth::check()) {
+            $userName = Auth::user()->name;
+            return "Selamat Datang, " . $userName;
+        }
+
+        return "Tidak ada pengguna yang login saat ini.";
     }
 }

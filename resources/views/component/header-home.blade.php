dcd991c5ce0728f2bd8cf375193b22d4a0861f20
rafiramdhanpr <rafiramdhanpr@gmail.com>
2024-05-28 16:23:32 +0700

    memperbaiki kode header

--- resources/views/component/header-home.blade.php
+++ resources/views/component/header-home.blade.php
@@ -2,7 +2,7 @@
     <a href="home">
     <img class="img" src="assets/group-4.png" />
     <div class="auto-layout">
-        <a href="" class="text-wrapper-5">Beranda</a>
+        <a href="" class="text-wrapper-5">Beranda</a>     
         <a href="footer" class="text-wrapper-6">Kontak</a>
         <div class="group-wrapper">
             <div class="group-2">
